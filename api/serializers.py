452ec9f6bdacbd5e239ba66619f515720bc9cb00
romangrbr <romangrbr@gmail.com>
2026-03-10 23:29:36 +0300

    add views and serializers

---
+++ api/serializers.py
@@ -0,0 +1,151 @@
+from rest_framework import serializers
+
+from .models import Answer, Choice, Question, Survey, SurveySession
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ('id', 'text', 'order')
+        extra_kwargs = {'id': {'read_only': False, 'required': False}}
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ('id', 'text', 'order', 'choices')
+
+
+class QuestionWriteSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True, required=False)
+
+    class Meta:
+        model = Question
+        fields = ('id', 'text', 'order', 'choices')
+
+    def _save_choices(self, question: Question, choices_data: list) -> None:
+        existing_ids = {c['id'] for c in choices_data if c.get('id')}
+        question.choices.exclude(id__in=existing_ids).delete()
+
+        for data in choices_data:
+            choice_id = data.pop('id')
+            Choice.objects.update_or_create(
+                id=choice_id,
+                defaults={**data, 'question': question},
+            )
+
+    def create(self, validated_data):
+        choices_data = validated_data.pop('choices', [])
+        question = Question.objects.create(**validated_data)
+        self._save_choices(question, choices_data)
+        return question
+
+    def update(self, instance, validated_data):
+        choices_data = validated_data.pop('choices')
+        if choices_data is not None:
+            self._save_choices(instance, choices_data)
+        return super().update(instance, validated_data)
+
+
+class SurveyListSerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(source='author.username', read_only=True)
+    questions_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Survey
+        fields = ('id', 'title', 'author_username', 'created_at', 'questions_count')
+
+
+class SurveyDetailSerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(source='author.username', read_only=True)
+    questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Survey
+        fields = ('id', 'title', 'author_username', 'created_at', 'questions')
+
+
+class SurveyWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Survey
+        fields = ('id', 'title')
+
+    def create(self, validated_data: dict) -> Survey:
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class SurveySessionSerializer(serializers.ModelSerializer):
+    duration_seconds = serializers.FloatField(read_only=True)
+    is_completed = serializers.BooleanField(read_only=True)
+
+    class Meta:
+        model = SurveySession
+        fields = ('id', 'survey', 'started_at', 'completed_at', 'is_completed', 'duration_seconds')
+        read_only_fields = ('id', 'started_at', 'completed_at', 'is_completed', 'duration_seconds')
+
+    def validate_survey(self, survey: Survey) -> Survey:
+        user = self.context['request'].user
+        if user.survey_sessions.filter(survey=survey).exists():
+            raise serializers.ValidationError('Вы уже проходили этот опрос.')
+        return survey
+
+    def create(self, validated_data: dict) -> SurveySession:
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    choice = serializers.PrimaryKeyRelatedField(
+        queryset=Choice.objects.all(),
+    )
+
+    class Meta:
+        model = Answer
+        fields = ('id', 'session', 'question', 'choice', 'answered_at')
+        read_only_fields = ('id', 'answered_at')
+
+    def validate(self, attrs: dict) -> dict:
+        session  = attrs['session']
+        question = attrs['question']
+
+        if question.survey_id != session.survey_id:
+            raise serializers.ValidationError(
+                'Вопрос не принадлежит опросу данного сеанса.'
+            )
+
+        request = self.context['request']
+        if session.user_id != request.user.pk:
+            raise serializers.ValidationError('Это не ваш сеанс.')
+
+        if attrs['choice'].question_id != question.pk:
+            raise serializers.ValidationError(
+                f'Вариант {attrs["choice"].pk} не принадлежит вопросу {question.pk}.'
+            )
+
+        if session.answers.filter(question=question).exists():
+            raise serializers.ValidationError('Вы уже ответили на этот вопрос.')
+
+        return attrs
+
+
+class AnswerStatSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    choice_id = serializers.IntegerField()
+    choice_text = serializers.CharField()
+
+
+class QuestionStatSerializer(serializers.Serializer):
+    question_id = serializers.IntegerField()
+    question_text = serializers.CharField()
+    answers = AnswerStatSerializer(many=True)
+
+
+class SurveyStatSerializer(serializers.Serializer):
+    survey_id = serializers.IntegerField()
+    total_sessions = serializers.IntegerField()
+    completed_sessions = serializers.IntegerField()
+    avg_duration_seconds = serializers.FloatField(allow_null=True)
+    questions = QuestionStatSerializer(many=True)
---
+++ api/views.py
@@ -0,0 +1,208 @@
+from django.db.models import Avg, Count, DurationField, F, Prefetch
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from drf_spectacular.utils import extend_schema
+from rest_framework import generics, mixins, permissions, status, viewsets
+from rest_framework.response import Response
+
+from .permissions import IsAdminUser
+from .models import Answer, Question, Survey, SurveySession
+from .serializers import (
+    AnswerSerializer,
+    QuestionSerializer,
+    QuestionWriteSerializer,
+    SurveyDetailSerializer,
+    SurveyListSerializer,
+    SurveySessionSerializer,
+    SurveyStatSerializer,
+    SurveyWriteSerializer,
+)
+
+
+class SurveyViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, IsAdminUser]
+
+    def get_queryset(self):
+        return (
+            Survey.objects
+            .select_related('author')
+            .annotate(questions_count=Count('questions'))
+        )
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return SurveyListSerializer
+        if self.action in ('create', 'update', 'partial_update'):
+            return SurveyWriteSerializer
+        return SurveyDetailSerializer
+
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = (
+            Survey.objects
+            .prefetch_related('questions__choices')
+            .get(pk=kwargs['pk'])
+        )
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, IsAdminUser]
+
+    def get_queryset(self):
+        return (
+            Question.objects
+            .filter(survey_id=self.kwargs['survey_pk'])
+            .prefetch_related('choices')
+        )
+
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return QuestionSerializer
+        return QuestionWriteSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(survey=get_object_or_404(Survey, pk=self.kwargs['survey_pk']))
+
+
+class SurveySessionViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = SurveySessionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return (
+            SurveySession.objects
+            .filter(user=self.request.user)
+            .select_related('survey')
+        )
+
+
+
+class AnswerViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = AnswerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filterset_fields = ['session']
+
+    def get_queryset(self):
+        return (
+            Answer.objects
+            .filter(session__user=self.request.user)
+            .select_related('question', 'choice')
+        )
+
+    def perform_create(self, serializer):
+        answer = serializer.save()
+        session = answer.session
+        if session.answers.count() == session.survey.questions.count():
+            session.completed_at = timezone.now()
+            session.save(update_fields=['completed_at'])
+
+
+class NextQuestionView(generics.RetrieveAPIView):
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    @extend_schema(
+        summary='Следующий вопрос опроса',
+        description=(
+            'Возвращает следующий неотвеченный вопрос для текущего пользователя.'
+        ),
+        responses={
+            status.HTTP_200_OK: QuestionSerializer,
+            status.HTTP_204_NO_CONTENT: None,
+            status.HTTP_404_NOT_FOUND: None,
+        },
+    )
+    def get(self, request, *args, **kwargs):
+        survey_pk = self.kwargs['survey_pk']
+        session = (
+            SurveySession.objects
+            .filter(survey_id=survey_pk, user=request.user, completed_at__isnull=True)
+            .first()
+        )
+        if session is None:
+            return Response(
+                {'detail': 'Активный сеанс не найден.'},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        answered_question_ids = set(
+            session.answers
+            .values_list('question_id', flat=True)
+        )
+        next_question = (
+            Question.objects
+            .filter(survey_id=survey_pk)
+            .exclude(id__in=answered_question_ids)
+            .prefetch_related('choices')
+            .first()
+        )
+        if next_question is None:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        serializer = self.get_serializer(next_question)
+        return Response(serializer.data)
+
+
+class SurveyStatView(generics.RetrieveAPIView):
+    serializer_class = SurveyStatSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdminUser]
+
+    def get(self, request, *args, **kwargs):
+        survey = get_object_or_404(Survey, pk=kwargs['survey_pk'])
+
+        sessions_qs = survey.sessions.all()
+        total_sessions = sessions_qs.count()
+        completed_sessions = sessions_qs.filter(completed_at__isnull=False).count()
+        avg_data = sessions_qs.filter(completed_at__isnull=False).aggregate(
+            avg_sec=Avg(F('completed_at') - F('started_at'), output_field=DurationField())
+        )
+        avg_duration = None
+        if avg_data['avg_sec']:
+            avg_duration = avg_data['avg_sec'].total_seconds()
+
+        questions = (
+            survey.questions
+            .prefetch_related(
+                Prefetch(
+                    'answers',
+                    queryset=Answer.objects
+                        .filter(session__survey=survey)
+                        .select_related('session__user', 'choice'),
+                )
+            )
+        )
+
+        questions_stat = []
+        for q in questions:
+            questions_stat.append({
+                'question_id': q.pk,
+                'question_text': q.text,
+                'answers': [
+                    {
+                        'username': a.session.user.username,
+                        'choice_id': a.choice_id,
+                        'choice_text': a.choice.text,
+                    }
+                    for a in q.answers.all()
+                ],
+            })
+
+        data = {
+            'survey_id': survey.pk,
+            'total_sessions': total_sessions,
+            'completed_sessions': completed_sessions,
+            'avg_duration_seconds': avg_duration,
+            'questions': questions_stat,
+        }
+        serializer = self.get_serializer(data)
+        return Response(serializer.data)
