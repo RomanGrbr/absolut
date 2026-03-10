e917bcdd8f354d0070d30906599f3fb08912c4e4
romangrbr <romangrbr@gmail.com>
2026-03-10 23:28:21 +0300

    add admin

--- api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Survey, Question, Choice, SurveySession, Answer
-
-
-@admin.register(Survey, Question, Choice, SurveySession, Answer)
-class AllAdmin(admin.ModelAdmin):...
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'email', 'role')
+    list_display_links = ('username',)
