2e45cf82f9ca63dfdad9de5a81a9b97fa6b057fc
romangrbr <romangrbr@gmail.com>
2026-03-16 14:14:07 +0300

    ref permissions, delete is_authenticated

--- api/permissions.py
+++ api/permissions.py
@@ -3,11 +3,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminUser(BasePermission):
     def has_permission(self, request, view) -> bool:
-        return request.user and request.user.is_authenticated and request.user.is_admin
+        return request.user.is_admin
 
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view) -> bool:
-        if not (request.user and request.user.is_authenticated):
-            return False
         return request.method in SAFE_METHODS or request.user.is_admin
